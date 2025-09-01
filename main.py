7fb3bc757d0e35ac0618dd7517cbc75025cc98e0
Weibo0123 <weibolin322@gmail.com>
2025-09-01 15:30:26 -0500

    add get_disk_usage

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description="The system monitor")
     parser.add_argument("-c", "--cpu", action="store_true", help="check the CPU")
     parser.add_argument("-m", "--mem", action="store_true", help="check the Memory")
+    parser.add_argument("-d", "--disk", action="store_true", help="check the Disk")
 
     args = parser.parse_args()
 
@@ -12,11 +13,13 @@ def main():
 
     cpu_usage = get_cpu_usage()
     mem_usage = get_memory_usage()
+    disk_usage = get_disk_usage()
 
 
     if not any(args_dict.values()):
         print(f"CPU Usage: {cpu_usage}%" )
         print(f"Memory Usage: {mem_usage.percent}%")
+        print(f"Disk Usage: {disk_usage.percent}%")
     else:
         if args.cpu:
             print(f"CPU Usage: {cpu_usage}%" )
@@ -26,8 +29,13 @@ def main():
             print(f"Used: {mem_usage.used / (1024 ** 3):.2f} GB")
             print(f"Available: {mem_usage.available/ (1024 ** 3):.2f} GB")
             print(f"Usage: {mem_usage.percent}%")
-
-    
+        if args.disk:
+            print(f"Disk Usage:")
+            print(f"Total: {disk_usage.total / (1024 ** 3):.2f} GB")
+            print(f"Used: {disk_usage.used / (1024 ** 3):.2f} GB")
+            print(f"Free: {disk_usage.free/ (1024 ** 3):.2f} GB")
+            print(f"Usage: {mem_usage.percent}%")
+        
 
 
 def get_cpu_usage():
@@ -36,6 +44,10 @@ def get_cpu_usage():
 
 def get_memory_usage():
     usage = psutil.virtual_memory()
+    return usage
+
+def get_disk_usage():
+    usage = psutil.disk_usage("/")
     return usage
 
 if __name__ == "__main__":
