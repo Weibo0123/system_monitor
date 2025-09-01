ca97cae9cd41f026f4e949987f383154d320a564
Weibo0123 <weibolin322@gmail.com>
2025-08-31 20:31:03 -0500

    add get_memory_usage

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import psutil
 
 def main():
     cpu_usage = get_cpu_usage()
-    print(f"CPU usage: {cpu_usage}%" )
+    mem_usage = get_memory_usage()
+    print(f"CPU Usage: {cpu_usage}%" )
+    print(f"Memory Usage: {mem_usage}%")
+
 
 
 def get_cpu_usage():
     usage = psutil.cpu_percent(interval=1)
     return usage
+
+def get_memory_usage():
+    usage = psutil.virtual_memory()
+    return usage.percent
 
 if __name__ == "__main__":
     main()
