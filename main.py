fdb16651989ceb4e2b6266c33a9b9a24fa4cbfe7
Weibo0123 <weibolin322@gmail.com>
2025-09-02 21:07:36 -0500

    add the daemon mode

--- main.py
+++ main.py
@@ -8,29 +8,17 @@ def main():
     parser.add_argument("-m", "--mem", action="store_true", help="check the Memory")
     parser.add_argument("-d", "--disk", action="store_true", help="check the Disk")
     parser.add_argument("-n", "--net", action="store_true", help="check the Network")
+    parser.add_argument("-a", "--daemon", action="store_true", help="run in daemon mode(every 60s)")
+
 
     args = parser.parse_args()
 
-    args_dict = vars(args)
-
-    cpu_usage = get_cpu_usage()
-    each_core_of_cpu_usage = get_each_core_of_cpu_usage()
-    mem_usage = get_memory_usage()
-    disk_usage = get_disk_usage()
-    net_speed = get_net_speed()
-
-
-    if not any(args_dict.values()):
-        print_all_usage_percentage(cpu_usage, mem_usage, disk_usage, net_speed)
+    if args.daemon:
+        while True:
+            collect_args_and_print(args)
+            time.sleep(5)
     else:
-        if args.cpu:
-            print_cpu_usage(cpu_usage, each_core_of_cpu_usage)
-        if args.mem:
-            print_memory_usage(mem_usage)
-        if args.disk:
-            print_disk_usage(disk_usage)
-        if args.net:
-            print_net_speed(net_speed)
+        collect_args_and_print(args)
         
 
 
@@ -72,12 +60,14 @@ def print_all_usage_percentage(cpu, mem, disk, net):
         print(f"Memory Usage: {mem.percent}%")
         print(f"Disk Usage: {disk.percent}%")
         print(f"Download Speed: {net[1] / (1024 ** 2):.2f} MB")
+        print("")
 
 
 def print_cpu_usage(usage, cores):
     print(f"CPU Usage: {usage}%")
     for i, usage in enumerate(cores):
         print(f"CPU Core {i}: {usage}%")
+    print("")
 
 
 def print_memory_usage(mem):
@@ -86,6 +76,7 @@ def print_memory_usage(mem):
     print(f"Used: {mem.used / (1024 ** 3):.2f} GB")
     print(f"Available: {mem.available/ (1024 ** 3):.2f} GB")
     print(f"Usage: {mem.percent}%")
+    print("")
 
 
 def print_disk_usage(disk):
@@ -94,6 +85,7 @@ def print_disk_usage(disk):
     print(f"Used: {disk.used / (1024 ** 3):.2f} GB")
     print(f"Free: {disk.free/ (1024 ** 3):.2f} GB")
     print(f"Usage: {disk.percent}%")
+    print("")
 
 
 def print_net_speed(net):
@@ -102,7 +94,30 @@ def print_net_speed(net):
     print(f"Download Speed: {net[1] / (1024):.2f} KB/s")
     print(f"Packets Upload: {int(net[2])} Packets/s")
     print(f"Packets Download: {int(net[3])} Packets/s")
+    print("")
 
+
+def collect_args_and_print(args):
+    args_dict = vars(args).copy()
+    args_dict.pop("daemon", None)
+
+    cpu_usage = get_cpu_usage()
+    each_core_of_cpu_usage = get_each_core_of_cpu_usage()
+    mem_usage = get_memory_usage()
+    disk_usage = get_disk_usage()
+    net_speed = get_net_speed()
+    if not any(args_dict.values()):
+            print_all_usage_percentage(cpu_usage, mem_usage, disk_usage, net_speed)
+    else:
+        if args.cpu:
+            print_cpu_usage(cpu_usage, each_core_of_cpu_usage)
+        if args.mem:
+            print_memory_usage(mem_usage)
+        if args.disk:
+            print_disk_usage(disk_usage)
+        if args.net:
+            print_net_speed(net_speed)
+    
 
 if __name__ == "__main__":
     main()
